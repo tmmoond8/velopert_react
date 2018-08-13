ca8e4d379e673ae8ff3c81df16855bd28e9b8c4a
tmmoon <tmmoon@synapsoft.co.kr>
2018-08-13 13:15:24 +0900

    chapter2 jsx 기본

--- example1/src/App.js
+++ example1/src/App.js
@@ -1,19 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
+    const text = '당신은 어썸하신가요??';
+    const condition = true;
+    const style = {
+      backgroundColor: 'gray',
+      border: '1px solid black',
+      height: Math.round(Math.random() * 300) + 50,
+      width: Math.round(Math.random() * 300) + 50,
+      WebkitTransition: 'all',
+      MozTransition: 'all',
+      msTransition: 'all'
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>
+        <div className="my-div">
+          {/* {<Abc/>} */}
+          <h1> 리액트 안녕!</h1>
+          <h2> {text} </h2>
+          {
+            condition && '이것을 보여주세요'
+          }
+          <div style={style}
+            // 여기에서는 주석을 이렇게 사용할 수 있습니다
+            // skdjfljlfksaslda
+            // sdflsjsa
+          />
+        </div>
+      </Fragment>
     );
   }
 }
