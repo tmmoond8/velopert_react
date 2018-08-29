74687c0549652111cc4c0b32fd6e4a39534ecacd
tmmoon <tmmoon@synapsoft.co.kr>
2018-08-29 13:52:54 +0900

    chap9 open-color, include-media 추가

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import styles from './App.scss';
-import classnames from 'classnames/bind';
-const cx = classnames.bind(styles);
-
-console.log(styles);
+import Button from './components/Button';
 
 class App extends Component {
   render() {
-    const isBlue = true;
     return (
-      <div className={cx('box', { blue: isBlue })}>
-        <div className={cx('box-inside')}/>
+      <div>
+        <Button>버튼</Button>
       </div>
     );
   }
