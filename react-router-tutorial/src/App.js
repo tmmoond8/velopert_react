4e9d52a5e97633c07524b4b85ca09a76f275723c
문태민 <tmmoon@synapsoft.co.kr>
2018-09-20 13:28:26 +0900

    chap16 - 기본적인 라우팅

--- react-router-tutorial/src/App.js
+++ react-router-tutorial/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { Home, About } from 'pages';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      sdfdsf
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <Route exact path="/" component={Home}/>
+      <Route path="/about/:name" component={About}/>
+    </div>
+  );
 }
 
 export default App;
