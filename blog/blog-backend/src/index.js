c7ac170407fa716516c2e9f2072cca074de904ce
Taemin Moon <tmmoond8@gmail.com>
2018-09-23 12:03:25 +0900

    chap19 - mongoose, dotenv 설치

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+
+const { 
+  PORT: port = 4000,
+  MONGO_URI: mongoURI,
+} = process.env;
 
 const api = require('./api');
 const app = new Koa();
@@ -12,6 +18,6 @@ app.use(bodyParser());
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('listening to port 4000');
+app.listen(port, () => {
+  console.log('listening to port', port);
 });
