8b764080b048fe984448078d7ab35ccfd22166fa
Taemin Moon <tmmoond8@gmail.com>
2018-09-25 23:41:49 +0900

    chap21 - axios 추가 - editor header, post를 컨테이너로 감싸기

---
+++ blog/blog-frontend/src/containers/editor/EditorHeaderContainer.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import EditorHeader from 'components/editor/EditorHeader';
+import { withRouter } from 'react-router-dom';
+
+import * as editorActions from 'store/modules/editor';
+
+class EditorHeaderContainer extends Component {
+
+  componentDidMount() {
+    const { EditorActions } = this.props;
+    EditorActions.initialize();
+  }
+
+  handleGoBack = () => {
+    const { history } = this.props;
+    history.goBack();
+  }
+
+  handleSubmit = async () => {
+    const { title, markdown, tags, EditorActions, history } = this.props;
+    const post = {
+      title,
+      body: markdown,
+      tags: tags === "" ? [] : [...new Set(tags.split(',').map(tag => tag.trem()))]
+    };
+    try {
+      await EditorActions.writePost(post);
+      history.push(`/post/${this.props.postId}`);
+    } catch(e) {
+      console.error(e);
+    }
+  }
+
+  render() {
+    const { handleGoBack, handleSubmit } = this;
+
+    return (
+      <EditorHeader
+        onGoBack={handleGoBack}
+        onSubmit={handleSubmit}
+      />
+    );
+  }
+}
+
+export default connect(
+  (state) => ({
+    ...state,
+    title: state.editor.title,
+    markdown: state.editor.markdown,
+    tags: state.editor.tags,
+    postId: state.editor.postId
+  }),
+  (dispatch) => ({
+    EditorActions: bindActionCreators(editorActions, dispatch)
+  })
+)(withRouter(EditorHeaderContainer));
---
+++ blog/blog-frontend/src/store/modules/post.js
@@ -0,0 +1,25 @@
+import { createAction, handleActions } from 'redux-actions';
+
+import { pender } from 'redux-pender';
+import * as api from 'lib/api';
+
+const GET_POST = 'post/GET_POST';
+
+export const getPost = createAction(GET_POST, api.getPost);
+
+const initialState = {
+  post: {}
+}
+
+export default handleActions({
+  ...pender({
+    type: GET_POST,
+    onSuccess: (state, action) => {
+      const { data: post } = action.payload;
+      return {
+        ...state,
+        post
+      }
+    }
+  })
+}, initialState)
