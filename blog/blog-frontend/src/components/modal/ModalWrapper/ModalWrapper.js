33e1b265242a9d2e199a31efaf234b0371b84beb
tmmoon <tmmoond8@gmail.com>
2018-09-30 22:59:12 +0900

    chap21 - 모달 애니메이션 추가

--- blog/blog-frontend/src/components/modal/ModalWrapper/ModalWrapper.js
+++ blog/blog-frontend/src/components/modal/ModalWrapper/ModalWrapper.js
@@ -5,15 +5,43 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 class ModalWrapper extends Component {
+
+	state = {
+		animate: false
+	}
+
+	startAnimation = () => {
+		this.setState({
+			animate: true
+		});
+
+		setTimeout(() => {
+			this.setState({
+				animate: false
+			});
+		}, 250)
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (prevProps.visible !== this.props.visible) {
+			this.startAnimation();
+		}
+	}
+
 	render() {
 		const { children, visible } = this.props;
+		const { animate } = this.state;
+
+		if (!visible && !animate) return null;
+		const animation = animate && (visible ? 'enter' : 'leave');
+
 		return (
 			<div>
-				{!visible ? null : (
+				{!visible && !animate ? null : (
 					<Fragment>
-						<div className={cx('gray-background')}/>
+						<div className={cx('gray-background', animation)}/>
 						<div className={cx('modal-wrapper')}>
-							<div className={cx('modal')}>
+							<div className={cx('modal', animation)}>
 								{children}
 							</div>
 						</div>
