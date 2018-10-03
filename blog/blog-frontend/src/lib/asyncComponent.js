514008e40c2b72c924ad9f3475a997fddc07189d
Taemin Moon <tmmoond8@gmail.com>
2018-10-04 01:59:54 +0900

    chap21 - 코드 스플리팅

---
+++ blog/blog-frontend/src/lib/asyncComponent.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+export default function asyncComponet(getComponent) {
+  class AsyncComponet extends Component {
+    static Component = null;
+
+    state = { Component: AsyncComponet.Component };
+
+    constructor(props) {
+      super(props);
+      if (AsyncComponet.Component) return;
+      getComponent().then(({ default: Component }) => {
+        AsyncComponet.Component = Component;
+        this.setState({ Component });
+      });
+    }
+
+    render() {
+      const { Component } = this.state;
+      if (Component) {
+        return <Component { ...this.props } />
+      }
+      return null;
+    }
+  }
+ 
+  AsyncComponet.getComponent = () => {
+    return getComponent().then(({ default: Component }) => {
+      AsyncComponet.Component = Component;
+    });
+  }
+
+  return AsyncComponet;
+}
